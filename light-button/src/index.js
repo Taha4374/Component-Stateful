b7f5f0919aa398ffe49501f76c0ab802950186c2
Muhammad Taha <tahatariq367@gmail.com>
2023-08-24 08:27:48 -0700

    Fisrt=-Commit

---
+++ light-button/src/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+
+const Home = () => {
+
+  const [isLit, setLit] = useState(true);
+
+
+  return <div className={`room ${(isLit) ? "lit" : "dark"}`}>
+
+    <p> Light is {(isLit) ? "On" :  "Off"} </p>
+    {'\n'}
+
+    <button onClick={() => { setLit(!isLit) }}>
+      Turn {(isLit) ? "off" : "on"}
+    </button>
+  </div>
+}
+
+
+ReactDOM.render(<Home />, document.querySelector("#root"));
